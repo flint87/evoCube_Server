442c7dafb0169dbefff799c12ccd7afaa9a969bb
Georg Felgitsch <georg.felgitsch@gmail.com>
2014-06-30 14:27:33 +0200

    some additional logic added

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ app.use('/users', users);
 
 
 var ua = require('universal-analytics');
-var visitor = ua('UA-52372095-1').debug();
-
+var visitor = ua('UA-52372095-1');
+//var visitor = ua('UA-52372095-1').debug();
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 /// error handlers
@@ -45,67 +45,113 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 var server = app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + server.address().port);
 
-	
+
 	var sockets = io.listen(server);
 	console.log('Socket.io server listening on port ' + server.address().port);
+	var videoClient; //holds the socket ID of the newest video client
+	var remoteClient; //holds the socket ID of the newest remote client
+	var playList;
 
+	sockets.on("connection", function(socket) {
 
-	sockets.on("connection", function(socket){
-		
 		console.log("Connection " + socket.id + " accepted");
 		visitor.event("Connection Accepted", "Connection", "nice", 42).send();
+
+		socket.on("vote", function(vote) {
+
+		});
+
+		socket.on("disconnect", function() {
+			console.log("Connection " + socket.id + " terminated");
+		});
+
+
+		//##############################
+		//MESSAGES FROM REMOTE CLIENT
+		//#############################
+
+		socket.on("remote_playPause", function() {
+			visitor.event("RemoteEvent", "Play/Pause", "nice", 42).send();
+			console.log("Remote Play/Pause received");
+			videoClient.emit("playPause");
+
+		});
+
+		socket.on("remoteVolumeChange", function(newVolume) {
+			console.log("Remote VolumeChange received: " + newVolume);
+			videoClient.emit("volumeChange", newVolume);
+		});
+
+		//register the newest remote client to server
+		socket.on("remoteClientregister", function(fn) {
+			remoteClient = socket;
+			console.log("Remote Client registered with ID: " + remoteClient.id);
+			fn();
+			remoteClient.emit("sendPlayList", playList);
+
+		});
+
+		//get command from remote client to play a specific trailer
+		socket.on("playSpecifTrailer", function(trailerInternalName) {
+			console.log("Remote Client wants to stop: " + trailerInternalName);
+			videoClient.emit("playSpecifTrailer", trailerInternalName);
+
+		});
 		
-		socket.on("vote", function(vote){
-				
-		});
-		
-		socket.on("disconnect", function(){
-				console.log("Connection " + socket.id + " terminated");
-		});
+		//get command from remote client to play a specific trailer
+		socket.on("remoteStop", function() {
+			console.log("Remote Client wants to stop" );
+			videoClient.emit("stopTrailer");
 
-		socket.on("remote_playPause", function(){
-				visitor.event("RemoteEvent", "Play/Pause", "nice", 42).send();
-				console.log("Remote Play/Pause received");
-				sockets.emit("playPause");
-				
-		});
-
-		socket.on("remoteVolumeChange", function(newVolume){
-				console.log("Remote VolumeChange received: " + newVolume);
-				sockets.emit("volumeChange", newVolume);
-				
 		});
 
 		
-		socket.on("ticker", function(fn){
-			
+
+
+		//##############################
+		//MESSAGES FROM VIDEO CLIENT
+		//#############################
+
+		//register the newest video client to server
+		socket.on("videoClientregister", function(fn) {
+			videoClient = socket;
+			console.log("Video Client registered with ID: " + videoClient.id);
+			fn();
 		});
 
+		//receive the available playlist from the video client
+		socket.on("registerPlayList", function(newPlaylist, fn) {
+			console.log("Playlist received");
+			playList = newPlaylist;
+			for (var i = 0; i < newPlaylist.length; i++) {
+				console.log(newPlaylist[i].movieName);
+			}
+			fn();
+		});
 
 	});
-	
 
 });
 
--- public/javascripts/remote.js
+++ public/javascripts/remote.js
@@ -1,16 +1,15 @@
-
-
 var socket;
 
 function connect() {
 
 
 
-	//socket = io("192.168.0.29:3000");
-	socket = io("http://178.77.68.72:3000");
+	socket = io("192.168.0.29:3000");
+	//socket = io("http://178.77.68.72:3000");
 
 	socket.on("connect", function() {
 		$("#status").html("Connected to Server");
+		registerToServer();
 	});
 	socket.on("disconnect", function() {
 		$("#status").html("Disconnected from Server");
@@ -27,6 +26,23 @@ function connect() {
 		socket.emit("remoteVolumeChange", $("#volume").val());
 	});
 
+	socket.on("sendPlayList", function(newPlaylist) {
+		console.log("Playlist successfully received from Server");
+		for (var i = 0; i < newPlaylist.length; i++) {
+			$('#movieList').append('<li id= \'' + newPlaylist[i].interalName + '\'><a>' + newPlaylist[i].movieName + '</a></li>').listview('refresh');
+		}
+		$('#movieList').on('click', 'li', function() {
+			console.log($(this).attr("id"));
+			socket.emit("playSpecifTrailer", $(this).attr("id"));
+		});
+	});
+
+}
+
+
+function stopVideo(){
+	console.log("Stop pressed");
+	socket.emit("remoteStop");
 }
 
 
@@ -41,4 +57,12 @@ function start() {
 	console.log("Play/Pause pressed");
 	socket.emit("remote_playPause");
 
+}
+
+//notify server that this is the new remote client
+//only one remote client allowed to control the video 
+function registerToServer() {
+	socket.emit("remoteClientregister", function() {
+		console.log("Remote Client successfully registered to Server");
+	});
 }
